dd6337bc92a0218f0e804d0e13d149fb50f55130
NIKHIL PAULZAGDE <118707097+NikhilPaulzagde@users.noreply.github.com>
2023-11-27 12:38:40 +0530

    table uploaded

---
+++ SQL Projects/Indian Census Population/output1.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `Dataset1` (
+	`District` VARCHAR(27) NOT NULL, 
+	`State` VARCHAR(27) NOT NULL, 
+	`Growth` DECIMAL(38, 2) NOT NULL, 
+	`Sex_Ratio` DECIMAL(38, 0) NOT NULL, 
+	`Literacy` DECIMAL(38, 2) NOT NULL
+);
